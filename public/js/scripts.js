a653472a02e5e9d2442db951dd4d31ab5d965ff3
yonndenila97 <s223572679@deakin.edu.au>
2023-04-18 09:31:48 +0800

    changes in server js and additional code in script.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -17,7 +17,24 @@ const cardList = [
 ]
 
 const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
+    console.log("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const addCards = (items) => {
+    console.log(items);
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.desciption + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend);
+
+    });
+
 }
 
 const submitForm = () => {
@@ -30,32 +47,35 @@ const submitForm = () => {
     console.log("Form Data Submitted: ", formData);
 }
 
-const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-        '</div></div></div>';
-        
-      $("#card-section").append(itemToAppend)
-
-    });
-
+const getCats = () => {
+    $.get('/api/Cats',(response) => {
+        if(response.statusCode===200){
+            addCards(response.data);
+        }
+    })
 }
+
+// $(document).ready(function(){
+//     $('.materialboxed').materialbox();
+//     $('#formSubmit').click(()=>{
+
+//         submitForm();
+//     })
+
+//     addCards(cardList);
+//     $('.modal').modal();
+
+//   });
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=>{
-
-        submitForm();
-    })
-
-    addCards(cardList);
     $('.modal').modal();
 
-  });
+    getCats();
 
+    $('#formSubmit').click(() => {
+        
+        submitForm();
+
+    })
+});
